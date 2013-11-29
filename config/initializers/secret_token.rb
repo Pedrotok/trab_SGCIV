541754e147a6ea92c4a31fc3f2acf5bb1354e54a
Pedro L. F <pedrohlf.df@gmail.com>
2013-11-28 23:44:04 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetoBD::Application.config.secret_token = '949b2b613efc37bd1f84aa59e85b22d0ac0cb211b4134e1ceb184a8a23d23f30584c575c7e3d5303f9733fdd0e0f5710297a6d9c0dec1d548d0e4fb78d7ca660'
